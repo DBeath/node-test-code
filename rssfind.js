b00d0582e3bc2c998baafbee7077b1f69dc6d263
David Beath <davidgbeath@gmail.com>
2014-06-11 15:51:56 +1200

    Updated rssfind to select comments feed

--- rssfind.js
+++ rssfind.js
@@ -1,17 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = 'https://davidbeath.com';
+var url = 'http://stratechery.com/';
+
+var re = new RegExp('Comments');
 
 request.get(url, function (err, response, body) {
   var $ = cheerio.load(body);
 
   console.log($('title').text());
-  console.log($('link').attr('type'));
+  console.log();
+
   $('link').each(function (i, elem) {
+
     if ($(this).attr('type') == 'application/rss+xml') {
+      var title = $(elem).attr('title');
+      if (re.test(title)) {
+        console.log('Comments feed: ' + $(this).attr('href'));
+      }
       console.log('Found RSS ' + $(this).attr('href'));
+      //console.log($(this));
     };
-    console.log($(this).attr('href'));
   });
 });
